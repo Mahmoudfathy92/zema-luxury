3871f4854d98c25722137e1570f33c26d661888e
Mahmoud Fathy <mahmoudfathy92@gmail.com>
2026-09-26 16:20:09 +0300

    fix(header): move login next to search, make language switcher minimalist globe icon, simplify bags nav item, and add no-cache headers

--- build_enhanced_store.py
+++ build_enhanced_store.py
@@ -35,6 +35,9 @@ template = '''<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
   <title>ZEMA | Contemporary Luxury Handbags & Accessories</title>
   <meta name="description" content="ZEMA - حقائب وإكسسوارات نسائية معاصرة وفاخرة صُممت بكل فخر في مصر." />
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Pragma" content="no-cache" />
+  <meta http-equiv="Expires" content="0" />
   
   <!-- Google Fonts: Cairo & Playfair Display -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
